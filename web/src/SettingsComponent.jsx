8ee4e6b85668950afe8e5bb0ed14cb9249bbdbc0
Dominique Sandoz <dominique.sandoz@gmail.com>
2018-09-15 21:18:07 +0200

    Update build

--- web/src/SettingsComponent.jsx
+++ web/src/SettingsComponent.jsx
@@ -12,96 +12,29 @@ import TextField from '@material-ui/core/TextField';
 
 class SettingsComponent extends React.Component {
   state = {
-    words: {
-      "ai": "nonono",
-      "mechanical engineering": "dsfds sfesfes dsfsd",
-      "artificial ": "something different ",
-    },
     currentMoo: '',
     currentPoo: ''
   };
 
-  componentDidMount() {
-    const words = localStorage.getItem('words');
-    if (words) {
-      this.setState({
-        words: JSON.parse(words)
-      });
-    }
-    else {
-      this.setState({
-        words: {
-          "blockchain": "unnecessary complication",
-          "machine intelligence": "overrated technology",
-          "artificial intelligence": "a bunch of if statements (AI)",
-          "machine learning": "machine slavery",
-          "algorithms": "unreadable code",
-          "computers": "rocks with lightning",
-          "computer": "a rock with lightning",
-          "statistical": "out of date",
-          "programmed": "broken",
-          "data": "random text",
-          "predications": "random guesses",
-    
-          /* made for https://www.reddit.com/r/linusrants/hot/ */
-          rant: "love-speech",
-          rants: "love-speeches",
-          fart: "look",
-          "call your mother a hamster": "congratulate your mother for making you",
-          stupidity: "geniousness",
-          snuffed: "overwhelmed",
-          "fucking asshole": "not the hero we need, but the hero we deserve",
-          "real toilet paper": "your code",
-          "I won't have splinters and ink up my arse": "projects would be much more innovative",
-          "die as babies": "become super popular",
-          "stupid to find a tit to suck on": "good to write such code",
-          "garbage": "on another level",
-          DAMMIT: "MAKE PEACE NOT WAR",
-          "either genius, or a seriously diseased mind. I can't quite tell which": "beyond genius",
-          clown: "right"
-        }
-      });
-    }
-  }
-
-  saveState() {
-    localStorage.setItem('words', JSON.stringify(this.state.words));
-  }
-
-  handleAdd(e) {
-    e.preventDefault();
-
-    let words = Object.assign(this.state.words);
-    words[this.state.currentMoo] = this.state.currentPoo;
+  handleAdd = (event) => {
+    event.preventDefault();
 
     this.setState({
-      words: words,
       currentMoo: '',
       currentPoo: ''
     });
 
-    this.saveState();
-  }
-
-  remove(moo) {
-    let words = Object.assign(this.state.words);
-    delete words[moo];
-
-    this.setState({
-      words: words
-    });
-
-    this.saveState();
-  }
+    this.props.add(this.state.currentMoo, this.state.currentPoo);
+  };
 
   render() {
-    const list = Object.entries(this.state.words).map(([moo, poo]) => {
+    const list = Object.entries(this.props.words).map(([moo, poo]) => {
       return (
         <TableRow key={moo}>
           <TableCell>{moo}</TableCell>
           <TableCell>{poo}</TableCell>
           <TableCell>
-            <IconButton onClick={(e) => this.remove(moo)} aria-label="Delete">
+            <IconButton onClick={(e) => this.props.remove(moo)} aria-label="Delete">
               <Icon>delete</Icon>
             </IconButton>
           </TableCell>
